bb62a110eca0cc4f80c471a9f3904ef52658a48f
InLoveWithJulia <javalang@list.ru>
2020-10-07 00:23:07 +0300

    #9

--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -1,18 +1,62 @@
 package com.example.sweater.service;
 
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
+
+    public void updateProfile(User user, String username, String password) {
+        String newUsername = user.getUsername();
+        String newPassword = user.getPassword();
+        boolean isUsernameChanged = ((username != null && !username.equals(newUsername)) ||
+                ((newUsername != null && !newUsername.equals(username))));
+        boolean isPasswordChanged = ((password != null && !password.equals(newPassword)) ||
+                ((newPassword != null && !newPassword.equals(password))));
+        if (isUsernameChanged) {
+            user.setUsername(username);
+        }
+        if (isPasswordChanged) {
+            user.setPassword(password);
+        }
+        userRepo.save(user);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
     }
 }
